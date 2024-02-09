57264c5175e7787d9ee38bbbcdb5c2a2c471d113
krist <kristinaskyba@gmail.com>
2024-02-09 17:58:39 +0100

    cat jumped in

--- src/automation/lesson11/Main.java
+++ src/automation/lesson11/Main.java
@@ -5,14 +5,14 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
 
-        TextPrinter.printCatAscii();
 
         System.out.println("Hej, friend! :) \nTry to play a game with me");
         String[] words = {"sho", "haha", "please"};
         System.out.println("Type: " + Arrays.toString(words));
 
         TextPrinter.readAndOutputText(words);
-        System.out.println();
+        TextPrinter.printCatAscii();
+
     }
 }
 
